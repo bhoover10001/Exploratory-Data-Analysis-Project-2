7436a78784cc98bec78f9474234743822ddb2161
bhoover10001 <bhoover@acteva.com>
2014-07-27 07:13:32 -0700

    Generated plot3.png and plot4.png added more comments.

--- plot3.R
+++ plot3.R
@@ -1,11 +1,16 @@
 library("ggplot2")
 
+# plots the total PM2.5 emissions from Baltimore City, by year, and source type
+
 plot3<- function () {
   NEI <<- readRDS("summarySCC_PM25.rds");
   summary<- ddply(NEI[NEI$fips == "24510", ], .(year, type), summarise, sumEmissions = sum(Emissions));
-  ggplot(summary) + geom_line(aes(x=year, y=sumEmissions, colour=factor(type))) + 
-  labs(colour="Source Type",
-       x ="Year",
-       y = "Amount of PM2.5 emitted (Tons)",
-       title = "Baltimore City Yearly PM2.5 Emissions by Source Type")
+  png("plot3.png")
+  print(ggplot(summary) + 
+          geom_line(aes(x=year, y=sumEmissions, colour=factor(type))) + 
+          labs(colour="Source Type",
+               x ="Year",
+               y = "Amount of PM2.5 emitted (Tons)",
+               title = "Baltimore City Yearly PM2.5 Emissions by Source Type\nData reported every 3 years"));
+  dev.off();
 }
--- plot4.R
+++ plot4.R
@@ -1,9 +1,15 @@
 library("ggplot2")
 
+# displays the US yearly PM2 emissions from Coal Combustion sources
+
 plot4<- function () {
   NEI <<- readRDS("summarySCC_PM25.rds");
   SCC <- readRDS("Source_Classification_Code.rds");
   summary<- ddply(NEI[NEI$SCC %in% SCC$SCC[grep("coal", SCC$EI.Sector, ignore.case=TRUE)], ], .(year), summarise, sumEmissions = sum(Emissions));
-  plot(summary, xlab="Year", ylab="Amount of PM2.5 emitted (Tons)", main="US Yearly PM2.5 Emissions from Coal Combustion-Related Sources", type="n");
+  png("plot4.png")
+  plot(summary, xlab="Year", 
+        ylab="Amount of PM2.5 emitted (Tons)", 
+        main="US Yearly PM2.5 Emissions from Coal Combustion-Related Sources\nData reported every 3 years", type="n");
   lines(summary$year, summary$sumEmissions);
+  dev.off();
 }
